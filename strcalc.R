611ab5c2f65311961cd0a29989ef01f87d62ac99
lauren-a <lauren.g.ashman@gmail.com>
2016-04-12 15:51:55 +1000

    Use switch statement

--- strcalc.R
+++ strcalc.R
@@ -4,12 +4,8 @@ compute = function(input_string){
   num0 = as.integer(values[1])   #1st chr -> integer called num0
   operator = values[2]          #2nd chr -> called operator
   num1 = as.integer(values[3])   #3rd chr -> integer called num1
-  if (operator == '+'){
-    return(num0 + num1)
-  } else if (operator == '-'){
-    return(num0 - num1)
-  } else {
-    print("unknown operator!")
-    return(NA)
-  }
+  result <- switch(operator,
+                   '+' = num0 + num1,
+                   '-' = num0 - num1)
+  return(result)
 }
